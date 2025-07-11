2da627e127c90ec5c2854c8fb97ff7a062dd0a8e
SteveR <136388557+stevie912@users.noreply.github.com>
2025-07-11 01:28:11 +0000

    added footer content. Minor styling changes.

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,7 +1,42 @@
-<footer class="footer">    
-    <div class="row">
+<style>
+a:link {
+  color: lightcyan;
+}
+</style>
+<footer class="footer bg-secondary text-white">  
+    <div class="container padding-bottom-3x mb-2 mt-5">
+      <div class="row align-items-center">
+        <div class="col text-center">
+            <br>
+            <a href="#">Things to come</a>
+            <br>
+            <a href="#">Plans</a>
+            <br>
+            <a href="#">Ideas</a>
+        </div>
+        <div class="col text-center">
+            <br>
+            <a href="#">Things we're doing</a>
+            <br>
+            <a href="#">Current status</a>
+            <br>
+            <a href="#">Bookings</a>
+        </div>
+        <div class="col text-center">
+            <br>
+            <a href="#">Past greatness</a>
+            <br>
+            <a href="#">Photos</a>
+            <br>
+            <a href="#">Awards</a>
+        </div>
+      </div>
+
+    <div class="row ">
         <div class="col-lg-12 text-center">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
+            <br>
+            <p style="margin-bottom: -4px;"><small>This is the fine print. Anything may change at any time without warning. It's not our fault, you hired us.</small></p>
+            <p><small>Copyright &copy; <?php echo date('Y'); ?> </small></p>
         </div>
     </div>
 </footer>
